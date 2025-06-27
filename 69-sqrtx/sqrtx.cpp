5e1f691a5ac1230337ee5d9561547180f3bbe790
Sanjitha P <135594554+SanjithaP@users.noreply.github.com>
2025-06-27 19:23:48 -0400

    Time: 19 ms (7.09%) | Memory: 8.6 MB (50.86%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x>pow(2,31)-1) return 0;        
+        if(x>pow(2,31)-1) return 0;       
         for(long int i=1;i<=x;i++){
             if(i*i>x) return i-1;
             else if(i*i==x) return i;
