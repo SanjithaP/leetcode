c5de0f70eca50a34f81607784f22980c5ebed241
Sanjitha P <135594554+SanjithaP@users.noreply.github.com>
2026-02-07 15:11:06 -0500

    Time: 35 ms (37.10%) | Memory: 13.9 MB (93.63%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n= size(nums);
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};}
+            }
+        }
+        return {0};
+    }
+};
